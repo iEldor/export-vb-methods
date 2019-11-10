f5e2886a1fa4ac7149d50684ed83f87fb1872807
iEldor <ieldor@gmail.com>
2019-11-09 22:43:07 -0500

    Cleaned to function name, decreased lines by refactoring

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ root = 'C:/Users/eldor/Downloads/pytechdegree/export-vb-methods'
 
 # option to extract if/elseif validations along with method names
 include_validations = False
+if include_validations:
+    export = open('methods_with_validations.csv', 'a'); header = 'File, Method, Validation\n'
+else:
+    export = open('methods_names_only.csv', 'a'); header = 'File, Method\n'
+export.write(header)
 
-code = []
+# extract and write to file
 for dirpath, dirnames, filenames in os.walk(root):
     for filename in filenames:
         if filename.endswith('.vb'):
@@ -18,33 +23,11 @@ for dirpath, dirnames, filenames in os.walk(root):
                 for count, word in enumerate(words):
                     if include_validations:
                         if word in ['Sub', 'Function'] and count != len(words)-1:
-                            code.append({
-                                'file': filename
-                                ,'method': f'{word} {words[count + 1]}'
-                                ,'validation': ' '
-                            })
+                            word = f"{word} {words[count + 1].split('(')[0]}"
+                            export.write(f'{filename},{word}, \n')
                         elif word in ['If', 'ElseIf'] and count != len(words)-1:
-                            code.append({
-                                'file': filename
-                                ,'method': ' '
-                                ,'validation': ' '.join(words)
-                            })
+                            export.write(f"{filename}, ,{' '.join(words)}\n")
                     else:
                         if word in ['Sub', 'Function'] and count != len(words)-1:
-                            code.append({
-                                'file': filename
-                                ,'method': f'{word} {words[count + 1]}'
-                            })
-                    
-if include_validations:
-    file = open('methods_with_validations.csv', 'a')
-    header = 'File, Method, Validation\n'
-    file.write(header)
-    for count, method in enumerate(code):
-        file.write(f"{method['file']},{method['method']},{method['validation']}\n")
-else:
-    file = open('methods_names_only.csv', 'a')
-    header = 'File, Method\n'
-    file.write(header)
-    for count, method in enumerate(code):
-        file.write(f"{method['file']},{method['method']}\n")
+                            word = f"{word} {words[count + 1].split('(')[0]}"
+                            export.write(f'{filename},{word}\n')
